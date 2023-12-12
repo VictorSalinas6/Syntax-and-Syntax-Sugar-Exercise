f80a8a011981e781aab4f921a60c90ce7b4cbaf0
Victor Salinas <victorsalinas6@gmail.com>
2023-12-12 14:25:58 -0600

    Did the syntax sugar

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 11;
+            var response = (answer < 9) ? "is less than nine" : "is greater than or equal to nine";
+            Console.WriteLine($"{answer} {response}");
         }
     }
 }
